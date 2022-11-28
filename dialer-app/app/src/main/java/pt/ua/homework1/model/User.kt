9837b2cbb7c50e3f067065709f32a0d16a7983f9
Sevelantis <hamer223@o2.pl>
2022-11-28 23:27:13 +0000

    first

---
+++ dialer-app/app/src/main/java/pt/ua/homework1/model/User.kt
@@ -0,0 +1,10 @@
+package pt.ua.homework1.model
+
+import android.graphics.drawable.Drawable
+
+class User( val id : Int,
+            var name: String,
+            var phone: String,
+            var avatarImage: Int
+            ) : java.io.Serializable{
+}
