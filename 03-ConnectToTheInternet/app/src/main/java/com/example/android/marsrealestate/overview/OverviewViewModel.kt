c2e3119cc4702625b757ba77f799753f9af7537c
Sevelantis <hamer223@o2.pl>
2022-12-10 01:00:03 +0000

    Display URL images in RecyclerView UX

--- 03-ConnectToTheInternet/app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
+++ 03-ConnectToTheInternet/app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
@@ -20,30 +20,42 @@ package com.example.android.marsrealestate.overview
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.android.marsrealestate.network.MarsApi
+import com.example.android.marsrealestate.network.MarsProperty
+import kotlinx.coroutines.launch
 
-/**
- * The [ViewModel] that is attached to the [OverviewFragment].
- */
+enum class MarsApiStatus { LOADING, ERROR, DONE }
+
 class OverviewViewModel : ViewModel() {
 
-    // The internal MutableLiveData String that stores the most recent response
+    private val _status = MutableLiveData<MarsApiStatus>()
     private val _response = MutableLiveData<String>()
+    private val _properties = MutableLiveData<List<MarsProperty>>()
 
-    // The external immutable LiveData for the response String
     val response: LiveData<String>
         get() = _response
 
-    /**
-     * Call getMarsRealEstateProperties() on init so we can display status immediately.
-     */
+    val properties: LiveData<List<MarsProperty>>
+        get() = _properties
+
+    val status: LiveData<MarsApiStatus>
+        get() = _status
+
     init {
         getMarsRealEstateProperties()
     }
 
-    /**
-     * Sets the value of the status LiveData to the Mars API status.
-     */
     private fun getMarsRealEstateProperties() {
-        _response.value = "Set the Mars API Response here!"
+        viewModelScope.launch { // built-in coroutine
+            try {
+                _status.value = MarsApiStatus.DONE
+                _properties.value = MarsApi.retrofitService.getProperties()
+
+            } catch (e: Exception) {
+                _status.value = MarsApiStatus.ERROR
+                _properties.value = ArrayList()
+            }
+        }
     }
 }
